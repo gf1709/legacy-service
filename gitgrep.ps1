440647c64195138976c1a57c091b8f32429da0f2
fc0382 <gregorio.fracella@allitude.it>
2026-02-26 14:31:04 +0100

    grep

--- gitgrep.ps1
+++ gitgrep.ps1
@@ -8,17 +8,16 @@ function echo_ok {
     Write-Host $args[0] -BackgroundColor Blue
 }
 
-"start" | Out-File -FilePath res.txt -Encoding utf8
-
 $branches = git branch -a | Where-Object { $_ -match 'remotes' -and $_ -notmatch 'HEAD' }
 foreach ($branch in $branches) {
     $b = $branch -replace 'remotes/origin/', ''
+    $b = $b.Trim()
     if ($b -eq 'master') {
         echo_error "ignore master !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         continue
     }
     # echo_ok($branch)
-    echo_ok('current branch is ', $b)
+    # echo_ok('current branch is ', $b)
     $searchString = '\bif\b'
     git checkout $b
     $grepResult = Get-ChildItem -Recurse -Include *.java | Select-String -Pattern  $searchString
