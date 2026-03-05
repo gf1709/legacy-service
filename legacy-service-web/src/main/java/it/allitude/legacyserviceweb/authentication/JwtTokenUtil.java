0482da50e7ea6fc53112d94d8bfb290a3c05881c
fc0382 <gregorio.fracella@allitude.it>
2026-03-05 11:46:17 +0100

    JWT_TOKEN_VALIDITY: aumentata

--- legacy-service-web/src/main/java/it/allitude/legacyserviceweb/authentication/JwtTokenUtil.java
+++ legacy-service-web/src/main/java/it/allitude/legacyserviceweb/authentication/JwtTokenUtil.java
@@ -25,7 +25,7 @@ public class JwtTokenUtil implements Serializable {
 
 	private static final long serialVersionUID = -2550185165626007488L;
 	
-	public static final long JWT_TOKEN_VALIDITY = 5*60*60;
+	public static final long JWT_TOKEN_VALIDITY = 12*60*60;
 	private String getJWTSecret()
 	{
 		// return ConfigParms.getPropertyValue("jwt.secret");		
