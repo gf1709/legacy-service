57ce1ca970b59f3e83f074e8eec3fea307e18240
fc0382 <gregorio.fracella@allitude.it>
2026-03-09 17:03:20 +0100

    feat: enhance entitlement code generator with new record functionality and validation

--- client/legacy-service-fe/src/app/component/entitlement-code-generator/entitlement-code-generator.ts
+++ client/legacy-service-fe/src/app/component/entitlement-code-generator/entitlement-code-generator.ts
@@ -1,25 +1,28 @@
 import { Component, signal, WritableSignal } from '@angular/core';
 import { BkService } from '../../services/bk.service';
 import { MessageHelperService } from '../../services/message-helper.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 export class CSVRecord {
-    public description: string= '';
-    public nature: string= '';
-    public entitlement: string= '';
-    public code: string= '';
-    public application: string= '';
+  public description: string = '';
+  public nature: string = '';
+  public entitlement: string = '';
+  public code: string = '';
+  public application: string = '';
 }
 
 @Component({
   selector: 'app-entitlement-code-generator',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './entitlement-code-generator.html',
   styleUrl: './entitlement-code-generator.css',
 })
 export class EntitlementCodeGenerator {
 
-  records: WritableSignal<CSVRecord[]> = signal([]);
+  m_records: WritableSignal<CSVRecord[]> = signal([]);
+  m_toggleShowAddNewRecord: boolean = false;
 
   constructor(private bkService: BkService, private message_service: MessageHelperService) {
   }
@@ -29,6 +32,7 @@ export class EntitlementCodeGenerator {
 
 
   uploadListener($event: any): void {
+    this.m_toggleShowAddNewRecord = false;
     let text = [];
     let files = $event.srcElement.files;
     if (this.isValidCSVFile(files[0])) {
@@ -40,7 +44,7 @@ export class EntitlementCodeGenerator {
         let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);
         let headersRow = this.getHeaderArray(csvRecordsArray);
         let csvRecords: CSVRecord[] = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
-        this.records.set(csvRecords);
+        this.m_records.set(csvRecords);
       };
       reader.onerror = function () {
         console.log('error is occured while reading file!');
@@ -79,9 +83,61 @@ export class EntitlementCodeGenerator {
     return headerArray;
   }
   fileReset() {
-    // this.csvReader.nativeElement.value = "";
-    this.records.set([]);
+    if (!confirm('Sicuro di voler eliminare tutti gli entitlement ?'))
+      return
+    this.m_records.set([]);
+  }
+  toggleNewEntitlement() {
+    this.m_toggleShowAddNewRecord = !this.m_toggleShowAddNewRecord;
   }
 
+  m_description: string = '';
+  m_nature: string = '';
+  m_entitlement: string = '';
+  m_code: string = '';
+  m_application: string = '';
 
+  saveNewEntitlement() {
+    let csvRecord: CSVRecord = new CSVRecord();
+    csvRecord.description = this.m_description;
+    csvRecord.nature = this.m_nature;
+    csvRecord.entitlement = this.m_entitlement;
+    csvRecord.code = this.m_code;
+    csvRecord.application = this.m_application;
+    console.log('saveNewEntitlement...', csvRecord);
+
+    if (!csvRecord.description || !csvRecord.nature || !csvRecord.entitlement || !csvRecord.code || !csvRecord.application) {
+      this.message_service.messageShow(this.message_service.msg_type.Error, 'All fields are required');
+      return;
+    }
+    if (csvRecord.description.length < 3 || csvRecord.description.length > 20) {
+      this.message_service.messageShow(this.message_service.msg_type.Error, 'Description must be between 3 and 20 characters');
+      return;
+    }
+    if (csvRecord.nature.length < 3 || csvRecord.nature.length > 20) {
+      this.message_service.messageShow(this.message_service.msg_type.Error, 'Nature must be between 3 and 20 characters');
+      return;
+    }
+    if (csvRecord.entitlement.length < 3 || csvRecord.entitlement.length > 20) {
+      this.message_service.messageShow(this.message_service.msg_type.Error, 'Entitlement must be between 3 and 20 characters');
+      return;
+    }
+    if (csvRecord.code.length < 3 || csvRecord.code.length > 20) {
+      this.message_service.messageShow(this.message_service.msg_type.Error, 'Code must be between 3 and 20 characters');
+      return;
+    }
+    if (csvRecord.application.length < 3 || csvRecord.application.length > 20) {
+      this.message_service.messageShow(this.message_service.msg_type.Error, 'Application must be between 3 and 20 characters');
+      return;
+    }
+    this.m_records.update(records => [...records, csvRecord]);
+    this.m_description = '';
+    this.m_nature = '';
+    this.m_entitlement = '';
+    this.m_code = '';
+    this.m_application = '';
+    this.m_toggleShowAddNewRecord = !this.m_toggleShowAddNewRecord;
+
+  }
 }
+
